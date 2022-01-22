0025f4bd15127ead26c0753c8ed5f6df8f6aad27
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-01-22 15:43:27 -0300

    borradas pages

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -14,20 +14,11 @@ const routes: Routes = [
             (m) => m.OverviewPageModule
           ),
       },
-      {
-        path: 'tab1',
-        loadChildren: () =>
-          import('../tab1/tab1.module').then((m) => m.Tab1PageModule),
-      },
+
       {
         path: 'tab2',
         loadChildren: () =>
           import('../tab2/tab2.module').then((m) => m.Tab2PageModule),
-      },
-      {
-        path: 'tab3',
-        loadChildren: () =>
-          import('../tab3/tab3.module').then((m) => m.Tab3PageModule),
       },
       {
         path: '',
