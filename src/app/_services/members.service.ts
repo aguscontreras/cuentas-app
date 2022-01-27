1b69070645646f8e738ef8806a2a12f1bd8f8f23
aguscontreras <35529189+aguscontreras@users.noreply.github.com>
2022-01-26 23:15:14 -0300

    members de prueba

--- src/app/_services/members.service.ts
+++ src/app/_services/members.service.ts
@@ -12,5 +12,21 @@ export class MembersService {
   constructor() {
     this.membersSource = new BehaviorSubject<Member[]>([]);
     this.members$ = this.membersSource.asObservable();
+
+    const member_1 = new Member('Agustin', 100);
+    const member_2 = new Member('Mica', 200);
+    const member_3 = new Member('Manu', 320);
+    const member_4 = new Member('Nico', 470);
+    const member_5 = new Member('Mati', 70);
+    const member_6 = new Member('Cin', 88);
+
+    this.membersSource.next([
+      member_1,
+      member_2,
+      member_3,
+      member_4,
+      member_5,
+      member_6,
+    ]);
   }
 }
